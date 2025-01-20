9b955c6096ea7f9765a93c9093b0d9e75bfce8db
ThinBones <thinbones.jp@gmail.com>
2025-01-20 00:42:30 -0600

    Updated main file

--- Main.java
+++ Main.java
@@ -1,3 +1,26 @@
+import java.util.*;
+
 public class Main {
-	
+	private ArrayList<Task> taskList;
+	private Scanner scanner;
+
+	public static void main(String[] args) {
+		greet();
+		while (true) {
+			askOptions();
+		}
+	}
+
+	public static void greet() {
+		System.out.println("Hello there!");
+	}
+
+	public static void askOptions() {
+		System.out.println("What would you like to do?");
+		System.out.println("1. Make task.\n2. Clear task.\n" +
+				"3. View tasks.\n4. Edit task.\n5. Exit.");
+		int resp = scanner.nextInt();
+	}
+
+
 }
