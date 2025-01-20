684b609666e408783c56712753e2bb82593e315e
ThinBones <thinbones.jp@gmail.com>
2025-01-20 01:56:20 -0600

    Updated Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 import java.util.*;
 
 public class Main {
-	private ArrayList<Task> taskList;
-	private Scanner scanner;
+	private static ArrayList<Task> taskList;
+	private static Scanner scanner;
 
 	public static void main(String[] args) {
+		scanner = new Scanner(System.in);
+		taskList = new ArrayList<>();
 		greet();
 		while (true) {
 			askOptions();
@@ -20,7 +22,89 @@ public class Main {
 		System.out.println("1. Make task.\n2. Clear task.\n" +
 				"3. View tasks.\n4. Edit task.\n5. Exit.");
 		int resp = scanner.nextInt();
+		switch (resp) {
+			case 1:
+				makeTask();
+				break;
+			case 2:
+				clearTask();
+				break;
+			case 3:
+				viewTasks();
+				break;
+			case 4:
+				editTask();
+				break;
+			case 5:
+				scanner.close();
+				System.exit(0);
+		}
+	}
+	
+	public static void makeTask() {
+		String taskTime = "";
+		int hour = 0;
+		int minute = 0;
+		String taskName;
+		String taskDescription;
+		boolean validTime = false;
+
+		while (!validTime) {
+			System.out.println("What hour do you want this task to be done by?");
+			try {
+				hour = scanner.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Input an integer.");
+				scanner.nextLine();
+			}
+
+			System.out.println("What minute do you want this task to be done by?");
+			try {
+				minute = scanner.nextInt();
+			}
+			catch (InputMismatchException e) {
+				System.out.println("Input an integer.");
+				scanner.nextLine();
+			}
+
+			validTime = validTimeChecker(hour, minute);
+
+			if (!validTime) {
+				System.out.println("Time entered is not valid, "
+						+ "please try again.");
+			}
+		}
+
+		taskTime = hour + ":" + minute;
+
+		System.out.println("What should this task be named?");
+		taskName = scanner.next();	
+
+		System.out.println("Write a description for this task.");
+		taskDescription = scanner.next();
+		
+
+		taskList.add(new Task(taskTime, taskName, taskDescription));
 	}
 
+	public static boolean validTimeChecker(int hr, int min) {
+		if (hr < 24 && hr >= 0 && min < 60 && min >= 0) {
+			return true;
+		}
 
+		return false;
+	}
+
+	public static void clearTask() {
+		
+	}
+
+	public static void viewTasks() {
+
+	}
+
+	public static void editTask() {
+
+	}
 }
